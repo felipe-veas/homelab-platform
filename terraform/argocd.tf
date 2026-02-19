7ec09945d62ef0cd2ae11c8c4bf7fe4435cc1b78
Felipe Veas <146454314+felipe-veas@users.noreply.github.com>
2026-02-19 19:51:37 -0300

    feat: initial ArgoCD setup and core terraform configuration

---
+++ terraform/argocd.tf
@@ -0,0 +1,28 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = var.argocd_namespace
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = var.argocd_chart_version
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  values = [
+    file("${path.module}/../argocd/values.yaml")
+  ]
+
+  depends_on = [kubernetes_namespace.argocd]
+}
+
+resource "kubectl_manifest" "root_app" {
+  yaml_body = file("${path.module}/../argocd/root-app.yaml")
+
+  depends_on = [helm_release.argocd]
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "argocd_url" {
+  description = "ArgoCD UI URL"
+  value       = "http://argocd.localhost"
+}
+
+output "argocd_initial_password_command" {
+  description = "Command to retrieve the initial ArgoCD admin password"
+  value       = "kubectl -n ${var.argocd_namespace} get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d"
+}
+
+output "grafana_url" {
+  description = "Grafana UI URL"
+  value       = "http://grafana.localhost"
+}
+
+output "grafana_credentials" {
+  description = "Grafana default credentials"
+  value       = "admin / homelab"
+}
