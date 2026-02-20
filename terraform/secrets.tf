b38d3b420b4092a7ddb1dbfe5957ae5618166336
Felipe Veas <146454314+felipe-veas@users.noreply.github.com>
2026-02-19 22:11:36 -0300

    refactor(terraform): Update Kubernetes resource types

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_namespace" "monitoring" {
+resource "kubernetes_namespace_v1" "monitoring" {
   metadata {
     name = "monitoring"
     labels = {
@@ -7,10 +7,10 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-resource "kubernetes_secret" "grafana_admin" {
+resource "kubernetes_secret_v1" "grafana_admin" {
   metadata {
     name      = "grafana-admin-secret"
-    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    namespace = kubernetes_namespace_v1.monitoring.metadata[0].name
   }
 
   data = {
